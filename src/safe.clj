33bfc1079a0c9d05be5bb25771c9e746a8da7d53
Nihalia <filipwennerdahl@hotmail.com>
2016-12-18 23:43:11 +0100

    Updated for submission

--- src/safe.clj
+++ src/safe.clj
@@ -1,19 +1,13 @@
 (defmacro safe
-  "Does a thing while checking for exceptions."
   ([to-safe]
+   "Does a thing while checking for exceptions."
    `(try ~to-safe
          (prn ~to-safe)
-         (catch Exception ex# (prn ex#))))
-  ([variable expression]
-   (let [s (first variable) fr (second variable)]
-     `(try ~expression
-           (prn ~expression)
-           (catch Exception ex# (prn ex#)))))
-  )
-
-(import java.io.FileReader java.io.File)
-
-
-(safe (/ 15 5))
-(def v (safe [s (FileReader. (File. "C:\\Users\\woebi\\Downloads\\test.txt"))] (.read s)))
-v
+         (catch Exception e# (prn e#))))
+  ([vector exp]
+   (binding [vector exp]
+   `(try ~exp
+         (prn ~exp)
+         (catch Exception e# (prn e#))))))
+		 
+		 
