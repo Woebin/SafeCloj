50921b4978e0b00c2e39a3292e60b9d1a69a836c
Nihalia <filipwennerdahl@hotmail.com>
2016-12-18 23:46:15 +0100

    removed prn workaround for cursive

--- src/safe.clj
+++ src/safe.clj
@@ -2,12 +2,10 @@
   ([to-safe]
    "Does a thing while checking for exceptions."
    `(try ~to-safe
-         (prn ~to-safe)
-         (catch Exception e# (prn e#))))
+         (catch Exception e#)))
   ([vector exp]
    (binding [vector exp]
    `(try ~exp
-         (prn ~exp)
-         (catch Exception e# (prn e#))))))
+         (catch Exception e# )))))
 		 
 		 
